23d411086881cb303249654cd9d9458975872e88
max <max.tvn89@gmail.com>
2022-04-26 02:06:06 +0200

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t mosquitto-alpine:1.0 .
-docker run --name mosquitto-tls -v /home/parallels/docker/mosquitto-alpine/data:/etc/mosquitto/data -p 8883:8883 -p 1883:1883 -it -d mosquitto-alpine:1.0
+docker run --name mosquitto-tls -v /home/parallels/docker/mosquitto-alpine/data:/etc/mosquitto/data -p 8883:8883 -it -d mosquitto-alpine:1.0
